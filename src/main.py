843b9547f52ab2356ea5a247e13455ada79b480a
Tim Hanewich <tahanewich@live.com>
2025-03-26 20:39:14 -0400

    Built in governing layer

--- src/main.py
+++ src/main.py
@@ -17,17 +17,17 @@ while True:
 
     # apply a governor to the throttle
     governor:float = 0.5 # cut to X% power
-    throttle_percent = throttle_percent * governor
+    throttle_percent_governed:float = throttle_percent * governor
 
     # calcualte what duty cycle to enact, in nanoseconds
     # the standards for PWM motor input:
     # 1,000,000 nanoseconds = 1ms = the minimum throttle
     # 2,000,000 nanoseconds = 2ms = the maximum throttle
-    duty_ns:int = 1000000 + int(throttle_percent * 1000000)
-    duty_ns = min(max(duty_ns, 2000000), 1000000)
+    duty_ns:int = 1000000 + int(throttle_percent_governed * 1000000)
+    duty_ns = min(max(duty_ns, 1000000), 2000000) # min/max just to be sure we are not passing an invalid value to the motor
 
     # print?
-    print("Throttle Input = " + str(int(throttle_percent * 100)) + "%, duty in nanoseconds = " + str(duty_ns))
+    print("Throttle Input = " + str(int(throttle_percent * 100)) + "%, Throttle Input After Governor = " + str(int(throttle_percent_governed * 100)) + "% duty in nanoseconds = " + str(duty_ns))
 
     # apply
     pwm.duty_ns(duty_ns)
