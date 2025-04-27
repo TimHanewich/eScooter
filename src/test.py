5db828c9349d0f95811e313c77a985f9bde06763
Tim Hanewich <tahanewich@live.com>
2025-04-27 11:10:15 -0400

    Removed tests

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import machine
-import time
-
-adc = machine.ADC(26)
-
-while True:
-    val = adc.read_u16()
-    print(val)
-    time.sleep(0.25)
--- src/test2.py
+++
@@ -1,9 +0,0 @@
-import time
-import throttle
-
-t = throttle.Throttle(26)
-
-while True:
-    percent:float = t.read()
-    print(percent)
-    time.sleep(0.25)
